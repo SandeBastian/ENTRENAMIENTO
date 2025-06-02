2442cada3421f13c9a0eebfd3d3f3aed8c3360f6
SandeBastian <adrianjmf2006@gmail.com>
2025-06-02 18:51:41 -0500

    (ADD) Mode8

--- Mode7/slot1_2.cpp
+++ Mode7/slot1_2.cpp
@@ -1,3 +1,22 @@
-//
-// Created by Sebastian on 28/05/2025.
-//
+#include <iostream>
+#include <fstream>
+using namespace std;
+typedef char cadena[51];
+int main () {
+    int cod, n=2;
+    cadena name;
+    ofstream sal;
+    sal.open("datos1.txt");
+    if(!sal) {
+        cout<<"Error de apertura..."<<endl;
+    }else {
+        for(int i=0; i<n; i++) {
+            cout<<"Dato Nro "<<i<<":"<<endl;
+            cout<<"Codigo: "; cin >> cod;
+            cout<<"Nombre completo: "; cin>>name;
+            sal<<cod<<"\t"<<name<<endl;
+        }
+        sal.close();
+        cout<<"Proceso terminado..."<<endl;
+    }
+}
