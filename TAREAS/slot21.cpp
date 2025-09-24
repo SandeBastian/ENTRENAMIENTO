6038d8ae2e6236343bd3d65e0fdc83af36812db0
carlosmurillol-max <carlos.murillo.l@uni.pe>
2025-09-24 17:20:39 -0500

    (ADD) cambios desde otra pc

--- TAREAS/slot21.cpp
+++ TAREAS/slot21.cpp
@@ -1,3 +1,63 @@
-//
-// Created by Usuario on 24/09/2025.
-//
+/*
+Tarea 23: Algoritmo para hallar ventas máximas y mínimas de un tienda comercial de instrumentos musicales
+Una empresa comercializadora de instrumentos musicales tiene las ventas mensuales en dinero, por cada tipo de
+instrumentos que vende (Cuerda, Viento, Percusión y electrónicos) y cada una de las 5 tiendas con las que opera.
+Desarrollar un algoritmo que determine cuál fue el mes y tienda en que ocurrio la mayor venta total y cuál fue
+la línea de instrumentos ytienda que tuvo la menor venta anual. Utilice un arreglo tridimensional, tomando la
+sugerencia dada en clase y que figura en el material.
+*/
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+int main() {
+    int i, j;
+    float ventas[5][13], max, min;
+    for (j=1;j<13;j++) {
+        ventas[0][j]=0;
+    }
+    for (i=1;i<5;i++) {
+        ventas[i][0]=0;
+    }
+    for (i=1;i<5;i++) {
+        for (j=1;j<13;j++) {
+            ventas[i][j]=10000+rand()%90000;
+            ventas[0][j]=ventas[0][j]+ventas[i][j];
+            ventas[i][0]=ventas[i][0]+ventas[i][j];
+        }
+    }
+    max=ventas[0][1];
+    for (j=2;j<13;j++) {
+        if (ventas[0][j]>max) {
+            max=ventas[0][j];
+        }
+    }
+
+    //impresion
+    ventas[0][0]=100001;
+    for (i=0;i<5;i++) {
+        for (j=0;j<13;j++) {
+            cout<<ventas[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+    cout<<"\nMeses con mayores ventas: ";
+    for (j=1;j<13;j++) {
+        if (ventas[0][j]==max) {
+            cout<<j<<" ";
+        }
+    }
+    min=ventas[1][0];
+    for (i=2;i<5;i++) {
+        if (ventas[i][0]<min) {
+            min=ventas[i][0];
+        }
+    }
+    cout<<"\nTipo de instrumento con menor venta anual: ";
+    for (i=1;i<5;i++) {
+        if (ventas[i][0]==min) {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
