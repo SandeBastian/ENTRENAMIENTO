bf9666c7a85043fa6b22ba8fe5f16b0c0961f0a8
carlosmurillol-max <carlos.murillo.l@uni.pe>
2025-09-24 16:36:44 -0500

    (ADD) cambios desde otra pc

--- TAREAS/slot20.cpp
+++ TAREAS/slot20.cpp
@@ -3,3 +3,37 @@ Tarea 21: Algoritmo para determinar la suma de los elementos de las dos diagonal
 Desarrollar un algoritmo que lea una matriz cuadrada de orden n impar, donde n >= 5 y determine la suma de
 los elementos que figuran en sus dos diagonales principales.
 */
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    do {
+        cout<<"Ingrese el orden 'n' de la matriz (impar y n>=5) ";
+        cin>>n;
+        if (n<5) cout<<"Error. Valor no valido."<<endl;
+    }while (n<5);
+
+    int mat[n][n];
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            cout<<"mat ["<<i<<"]["<<j<<"]: ";
+            cin>>mat[i][j];
+        }
+    }
+    int suma=0;
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            if (i==j||i+j==n-1) {
+                suma=suma+mat[i][j];
+            }
+        }
+    }
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            cout<<mat[i][j]<<" ";
+            if (j==n-1) cout<<endl;
+        }
+    }
+    cout<<"\nLa suma es: "<<suma<<endl;
+    return 0;
+}
