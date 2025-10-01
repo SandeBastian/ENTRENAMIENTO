853003d9449efd28f3aa0e221b689f9e6faa7aa1
El Regente <elregente@gmail.com>
2025-10-01 16:02:02 -0500

    (ADD)

--- Mode2/PC2_P2_2024_II.cpp
+++ Mode2/PC2_P2_2024_II.cpp
@@ -1,3 +1,26 @@
+/*
+PC2 2024-II UNI-II FIIS
+Pregunta 2 (de 9:10 a 9:43 am) 7 puntos
+Se desea leer el código (entero de 3 cifras) “codigo” y el precio unitario (real positivo) de un conjunto de
+n (n<=200) productos. Se sabe que los codigo se pueden repetir (es información histórica). Se pide diseñar
+un algoritmo que lea esta información y luego:
+Calcule el precio promedio por cada producto “promedio” y las  veces que el precio de cada producto estuvo
+por debajo su promedio “# de rebajas”.
+Muestre un listado indicando el “codigo”, el “promedio” y el “# de rebajas”. Este listado debe mostrar sólo
+los productos que hayan tenido rebajas (“# de rebajas” > 0) y debe  estar ordenado en base al código (crecientemente).
+  Entrada:                Salida:
+  Codigo  Precio          Codigo    Promedio    #_de_rebajas
+  100     25.3            100       26.225      2
+  200     15.6            300       15.75       1
+  100     30.7            400       18          1
+  300     16.3
+  100     28.4
+  500     20.5
+  300     15.2
+  100     20.5
+  400     15.2
+  400     20.8
+*/
 #include <iostream>
 using namespace std;
 
